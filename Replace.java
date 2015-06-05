c46d29e438ac96a4166fe2dd0d5ebc0518909b29
hejianDK <hejianDK@live.cn>
2015-06-05 13:39:20 -0400

    2nd commit

---
+++ Replace.java
@@ -0,0 +1,21 @@
+package assignment3;
+
+public class Replace {
+	static String a = "abcdefg";
+
+	public static void main(String[] args) {
+		CharSequence b = "ab";
+		CharSequence c = "mn";
+		System.out.println(replace(b, c));
+	}
+
+	public static String replace(CharSequence str1, CharSequence str2) {
+		String c = "";
+		int len = str1.length();
+		for (int i = 0; i < a.length(); i++) {
+			a
+		}
+		return c;
+	}
+
+}
